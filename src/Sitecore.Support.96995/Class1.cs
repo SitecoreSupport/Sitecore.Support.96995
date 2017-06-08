ff9f6c4e231994c4d7a4726bfae14121c8f1c1b2
Evgen Savinov <evs@sitecore.net>
2017-06-08 17:19:20 +0300

    initial commit

---
+++ src/Sitecore.Support.96995/Class1.cs
@@ -0,0 +1,45 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Mvc.Configuration;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.IO;
+using System.Linq;
+
+
+namespace Sitecore.Support.Mvc.Pipelines.HttpRequest
+{
+    [UsedImplicitly]
+    public class TransferMvcLayout : HttpRequestProcessor
+    {
+        private static readonly string[] externalPathSchemes = new string[]
+        {
+            "http://",
+            "https://"
+        };
+
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            string filePath = Context.Page.FilePath;
+            if (!Sitecore.Mvc.Extensions.StringExtensions.IsWhiteSpaceOrNull(filePath) && !TransferMvcLayout.externalPathSchemes.Any((string p) => filePath.StartsWith(p)))
+            {
+                string text;
+                try
+                {
+                    text = Path.GetExtension(filePath);
+                }
+                catch (ArgumentException)
+                {
+                    text = string.Empty;
+                }
+                if (MvcSettings.IsViewExtension(text) || !Sitecore.Mvc.Extensions.StringExtensions.IsAbsoluteViewPath(filePath))
+                {
+                    Tracer.Info("MVC Layout detected - transfering to ASP.NET MVC");
+                    args.Context.Items["sc::IsContentUrl"] = "true";
+                    args.AbortPipeline();
+                }
+            }
+        }
+    }
+}
